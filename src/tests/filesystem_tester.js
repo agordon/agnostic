5dedb81294fcde5a4e5a5a917112fd13d71bd1ff
A. Gordon <assafgordon@gmail.com>
2014-05-29 22:33:25 -0400

    Filesystem - added remove/opendir/validation

--- src/tests/filesystem_tester.js
+++ src/tests/filesystem_tester.js
@@ -33,7 +33,54 @@ assert.strictEqual( t[1], "world");
 assert.throws( function(){ fs.mkdir("/tmp/"); } );
 assert.throws( function(){ fs.mkdir("/tmp"); } );
 assert.throws( function(){ fs.mkdir("/tmp/foo.txt"); } );
+assert.throws( function(){ fs.opendir("/tmp/bar"); } );
+assert.throws( function(){ fs.opendir("/tmp/foo.txt"); } );
 assert.throws( function(){ fs.openfile("/tmp",false); } );
 assert.throws( function(){ fs.openfile("/tmp",true); } );
 assert.throws( function(){ fs.openfile("/tmp/",true); } );
 assert.throws( function(){ fs.openfile("/foo/bar.txt",true); } );
+
+
+// Test 3 - Read-Directory
+fs.mkdir("/usr");
+fs.mkdir("/usr/bin");
+fs.openfile("/usr/bin/sort",true);
+fs.openfile("/usr/bin/cut",true);
+fs.mkdir("/usr/bin/test1");
+fs.openfile("/usr/bin/less",true);
+fs.openfile("/usr/bin/sh",true);
+fs.openfile("/usr/bin/cat",true);
+fs.mkdir("/usr/bin/test2");
+var o = fs.readdir("/usr/bin/");
+VerifyArray(t);
+//order should be the same as creation order
+assert.strictEqual( o[0], "/usr/bin/sort");
+assert.strictEqual( o[1], "/usr/bin/cut");
+assert.strictEqual( o[2], "/usr/bin/test1");
+assert.strictEqual( o[3], "/usr/bin/less");
+assert.strictEqual( o[4], "/usr/bin/sh");
+assert.strictEqual( o[5], "/usr/bin/cat");
+assert.strictEqual( o[6], "/usr/bin/test2");
+
+//Test 4 - opendir
+var dd = fs.opendir("/usr/bin");
+var st = dd.stat();
+assert.strictEqual ( st["object_type"], "directory" );
+
+
+//Test 5 - remove
+fs.remove("/usr/bin/cut");
+assert.throws( function(){ fs.remove("/tmp"); } );
+assert.throws( function(){ fs.remove("/usr/bin/foobar"); } );
+assert.throws( function(){ fs.remove("/usr"); } );
+assert.throws( function(){ fs.remove("/bin"); } );
+fs.remove("/usr/bin/sort");
+fs.remove("/usr/bin/sh");
+fs.remove("/usr/bin/test1");
+fs.remove("/usr/bin/test2");
+fs.remove("/usr/bin/less");
+fs.remove("/usr/bin/cat");
+fs.remove("/usr/bin"); // directory empty - can be removed
+
+
+
