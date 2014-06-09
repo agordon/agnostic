6936910f966b0ef7a510fe4757fc017b278b14bb
A. Gordon <assafgordon@gmail.com>
2014-06-09 19:38:50 -0400

    Browserify: utils/string_utils

--- src/tests/string_utils_tester.js
+++ src/tests/string_utils_tester.js
@@ -8,38 +8,38 @@
  */
 
 var assert = require('assert');
-require('utils/string_utils');
+var str_utils = require('utils/string_utils');
 
 var t = "hello";
 
-assert.ok ( t.startsWith("he") ) ;
-assert.ok ( t.startsWith("h") ) ;
-assert.ok ( t.startsWith("hello") ) ;
-assert.ok ( t.startsWith("") ) ;
-assert.ok ( !t.startsWith("hellow") ) ;
-assert.ok ( !t.startsWith("a") ) ;
-assert.ok ( !t.startsWith("help") ) ;
+assert.ok( str_utils.startsWith(t,"he") ) ;
+assert.ok( str_utils.startsWith(t,"h") ) ;
+assert.ok( str_utils.startsWith(t,"hello") ) ;
+assert.ok( str_utils.startsWith(t,"") ) ;
+assert.ok( !str_utils.startsWith(t,"hellow") ) ;
+assert.ok( !str_utils.startsWith(t,"a") ) ;
+assert.ok( !str_utils.startsWith(t,"help") ) ;
 
-assert.ok ( t.endsWith("o") ) ;
-assert.ok ( t.endsWith("lo") ) ;
-assert.ok ( t.endsWith("hello") ) ;
-assert.ok ( t.endsWith("") ) ;
-assert.ok ( !t.endsWith("whello") ) ;
-assert.ok ( !t.endsWith("w") ) ;
+assert.ok( str_utils.endsWith(t,"o") ) ;
+assert.ok( str_utils.endsWith(t,"lo") ) ;
+assert.ok( str_utils.endsWith(t,"hello") ) ;
+assert.ok( str_utils.endsWith(t,"") ) ;
+assert.ok( !str_utils.endsWith(t,"whello") ) ;
+assert.ok( !str_utils.endsWith(t,"w") ) ;
 
 var s = "" ;
-assert.ok ( s.startsWith("") ) ;
-assert.ok ( !s.startsWith("f") ) ;
-assert.ok ( s.endsWith("") ) ;
-assert.ok ( !s.endsWith("f") ) ;
+assert.ok ( str_utils.startsWith(s,"") ) ;
+assert.ok ( !str_utils.startsWith(s,"f") ) ;
+assert.ok ( str_utils.endsWith(s,"") ) ;
+assert.ok ( !str_utils.endsWith(s,"f") ) ;
 
 
 /* Test trimWhitespace() */
 
-assert.strictEqual(  "hello".trimWhitespace(),		"hello" ) ;
-assert.strictEqual(  "hello   \t ".trimWhitespace(),	"hello" ) ;
-assert.strictEqual(  "\t  \t\thello".trimWhitespace(),	"hello" ) ;
-assert.strictEqual(  "  hello  ".trimWhitespace(),	"hello" ) ;
-assert.strictEqual(  "\thello\t".trimWhitespace(),	"hello" ) ;
-assert.strictEqual(  "hello world".trimWhitespace(),	"hello world" ) ;
-assert.strictEqual(  "  hello\t world  ".trimWhitespace(),"hello\t world" ) ;
+assert.strictEqual(  str_utils.trimWhitespace("hello"),		"hello" ) ;
+assert.strictEqual(  str_utils.trimWhitespace("hello   \t "),	"hello" ) ;
+assert.strictEqual(  str_utils.trimWhitespace("\t  \t\thello"),	"hello" ) ;
+assert.strictEqual(  str_utils.trimWhitespace("  hello  "),	"hello" ) ;
+assert.strictEqual(  str_utils.trimWhitespace("\thello\t"),	"hello" ) ;
+assert.strictEqual(  str_utils.trimWhitespace("hello world"),	"hello world" ) ;
+assert.strictEqual(  str_utils.trimWhitespace("  hello\t world  "),"hello\t world" ) ;
